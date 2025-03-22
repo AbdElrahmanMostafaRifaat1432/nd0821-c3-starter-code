1e79b93f307e8dc42ea5aab36b590f76d0c237e6
AbdElrahmanMostafaRifaat1432 <bodymostafa2001@gmail.com>
2025-03-22 15:33:49 +0200

    cleaned census data and completed model.py code

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,16 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+import pandas as pd
+from ml.data import process_data
+from ml.model import train_model , inference , compute_model_metrics
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data = pd.read_csv("../data/census.csv")
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -23,6 +29,12 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
+joblib.dump(model, "model.joblib")
